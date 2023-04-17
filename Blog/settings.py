15fb7921a53323763af330eee2297c99601e18f2
Piero Salazar <psalazar@ide-solution.com>
2023-04-17 17:55:38 -0500

    cambios

--- Blog/settings.py
+++ Blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#)+te)w_97#yc6vp1#g5nmqdf3fkbf$tp@7330ie*6qw2b2oz&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
